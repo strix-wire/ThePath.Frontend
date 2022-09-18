890af2d10c15a9f6c5de59f4c7099fefd20c4ff5
Vasily <vasily_pavlov_98@mail.ru>
2022-09-18 14:20:09 +0700

    Create logger instance. Delete async in unused method.

--- ThePath.Frontend/Controllers/HomeController.cs
+++ ThePath.Frontend/Controllers/HomeController.cs
@@ -6,9 +6,15 @@ public class HomeController : Controller
 {
     private readonly ILogger<HomeController> _logger;
 
-    [HttpGet]
-    public async Task<IActionResult> Index()
+    public HomeController(ILogger<HomeController> logger)
     {
+        _logger = logger;
+    }
+
+    [HttpGet]
+    public IActionResult Index()
+    {
+        _logger.LogTrace("Request to index successful");
         return View();
     }
 }
