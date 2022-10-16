4c48c0b415aec58d3df3b05d16a1e27672d112e1
Vasily <vasily_pavlov_98@mail.ru>
2022-10-16 14:14:13 +0700

    refactoring EntertainmentServiceCreateDto model

--- ThePath.Frontend/Models/Classes/Dto/EntertainmentServiceCreateDto.cs
+++ ThePath.Frontend/Models/Classes/Dto/EntertainmentServiceCreateDto.cs
@@ -1,24 +1,24 @@
 ﻿using ThePath.Frontend.Models.Enum;
 
-namespace ThePath.Frontend.Models.Classes.Dto
+namespace ThePath.Frontend.Models.Classes.Dto;
+
+public class EntertainmentServiceCreateDto
 {
-    public class EntertainmentServiceCreateDto
-    {
-        /// <summary>
-        /// Not used yet
-        /// </summary>
-        public Guid UserId { get; set; }
-        /// <summary>
-        /// In rub
-        /// </summary>
-        public long Price { get; set; }
-        public TypeEntertainment TypeEntertainment { get; set; }
-        public string? Details { get; set; }
-        public Area Area { get; set; }
-        /// <summary>
-        /// Maybe not worked with double
-        /// </summary>
-        public double Latitude { get; set; }
-        public double Longitude { get; set; }
-    }
+    /// <summary>
+    /// Not used yet
+    /// </summary>
+    public Guid UserId { get; set; }
+    public string Name { get; set; }
+    public string City { get; set; }
+    public long Price { get; set; }
+    public TypeEntertainment TypeEntertainment { get; set; }
+    public string? Details { get; set; }
+    public Area Area { get; set; }
+    /// <summary>
+    /// Maybe not worked with double
+    /// </summary>
+    public double Latitude { get; set; }
+    public double Longitude { get; set; }
+    public string? UrlImage { get; set; }
+    public byte? Ranking { get; set; }
 }
