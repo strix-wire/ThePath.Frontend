cac0c210c9e2149d6486c8999ac4096a7de2f9e3
Vasily <vasily_pavlov_98@mail.ru>
2022-10-20 23:42:46 +0700

    View ResultListEntertainment half ready and edits map.js

--- ThePath.Frontend/wwwroot/js/map.js
+++ ThePath.Frontend/wwwroot/js/map.js
@@ -22,3 +22,40 @@ let map = null
 
 //    map.on('click', onMapClick);
 //}
+
+function StartMap(entertainmentLookupDtoByTypeAndAreaAndPrice) {
+    let count = entertainmentLookupDtoByTypeAndAreaAndPrice.length //id.length
+    console.log(entertainmentLookupDtoByTypeAndAreaAndPrice);
+    map = L.map('map').setView([parseFloat(entertainmentLookupDtoByTypeAndAreaAndPrice[0].latitude), parseFloat(entertainmentLookupDtoByTypeAndAreaAndPrice[0].longitude)], 11)
+    
+    L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+        attribution:
+            '© <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+    }).addTo(map)
+
+    var markers = new L.MarkerClusterGroup();
+    var markersList = [];
+
+    function populate() {
+        for (var i = 0; i < count; i++) {
+            var m = new L.Marker([parseFloat(entertainmentLookupDtoByTypeAndAreaAndPrice[i].latitude), parseFloat(entertainmentLookupDtoByTypeAndAreaAndPrice[i].longitude)]).bindPopup(entertainmentLookupDtoByTypeAndAreaAndPrice[i].name).on("click", onMapClick);
+            markersList.push(m);
+            markers.addLayer(m);
+        }
+        return false;
+    }
+
+    populate();
+    map.addLayer(markers);
+    function onMapClick(e) {
+        var z;
+        for (var i = 0; i <= count; i++) {
+            if (e.latlng.lat == entertainmentLookupDtoByTypeAndAreaAndPrice[i].latitude & e.latlng.lng == entertainmentLookupDtoByTypeAndAreaAndPrice[i].longitude) {
+                z = i;
+            }
+        }
+
+        var url = $("#RedirectTo").val();
+        location.href = url + "?nameid=" + id[z];
+    }
+}
